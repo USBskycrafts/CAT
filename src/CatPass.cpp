d240af6f7995d1965ae0ad61d96f7499903de338
Junran Yang <jyq3198@moore.wot.eecs.northwestern.edu>
2022-10-09 22:21:57 -0500

    H3-v1

--- src/CatPass.cpp
+++ src/CatPass.cpp
@@ -27,6 +27,8 @@ namespace {
     unordered_map<Instruction*, BitVector> inst_bitvector;
     unsigned inst_count = 0;
     unordered_map<int, BitVector> label_set;
+    unordered_map<Instruction*, BitVector> gen, kill, in, out;
+    queue<Instruction*> q;
     set<StringRef> CAT_Function;
     CAT() : FunctionPass(ID) {}
 
@@ -58,38 +60,121 @@ namespace {
       if(fun_name == "CAT_new") {
         label_set[inst_label[inst]] |= inst_bitvector[inst];
       } else if(CAT_Function.count(fun_name)){
-        auto origin_inst = cast<CallInst>(inst->getArgOperand(0));
-        label_set[inst_label[origin_inst]] |= inst_bitvector[inst];
+        auto first_Op = inst->getOperand(0);
+        if(isa<PHINode>(first_Op)) {
+          auto phi = cast<PHINode>(first_Op);
+          for(unsigned i = 0; i < phi->getNumOperands(); i++) {
+            if(isa<Instruction>(phi->getOperand(i))) {
+              label_set[inst_label[cast<Instruction>(phi->getOperand(i))]] |= inst_bitvector[inst];
+            }
+          }
+        } else {
+            auto origin_inst = cast<Instruction>(inst->getArgOperand(0));
+            label_set[inst_label[origin_inst]] |= inst_bitvector[inst];
+        }
       }
     }
 
-    void printGenAndKill(Instruction *inst) {
-      errs() << "INSTRUCTION: " << *inst << "\n***************** GEN\n{\n";
+    void setGenAndKill(Instruction *inst) {
       StringRef fun_name;
       if(isa<CallInst>(inst)) fun_name = cast<CallInst>(inst)->getCalledFunction()->getName();
-      BitVector bit_vec; 
+      BitVector bit_vec(inst_count, false); 
       if(fun_name == "CAT_new") {
         bit_vec = label_set[inst_label[inst]];
-        errs() << " " << *inst << "\n";
+        gen[inst] = inst_bitvector[inst];
       } else if(CAT_Function.count(fun_name)) {
         CallInst *cal_inst = cast<CallInst>(inst->getOperand(0));
         bit_vec = label_set[inst_label[cal_inst]];
-        errs() << " " << *inst << "\n";
+        gen[inst] = inst_bitvector[inst];
+      } else {
+        gen[inst] = BitVector(inst_count, false);
       }
-      errs() << "}\n**************************************\n";
-      errs() << "***************** KILL\n{\n";
+
+
       if(fun_name == "CAT_new") {
         bit_vec ^= inst_bitvector[inst];
-        for(unsigned int i = 0; i < bit_vec.size(); i++) {
-          if(bit_vec[i] == true)errs() << " " << *label_inst[i] << "\n";
-        }
+        kill[inst] = bit_vec;
       } else if(CAT_Function.count(fun_name)) {
         bit_vec ^= inst_bitvector[inst];
-        for(unsigned int i = 0; i < bit_vec.size(); i++) {
-          if(bit_vec[i] == true)errs() << " " << *label_inst[i] << "\n";
+        kill[inst] = bit_vec;
+      } else {
+        kill[inst] = BitVector(inst_count, false);
+      }
+    }
+
+    
+    void setInAndOut(Function &F) {
+      for(auto &inst : instructions(F)) {
+        q.push(&inst);
+      } 
+      while(!q.empty()) {
+        auto &inst = q.front();
+        auto bb = inst->getParent();
+        q.pop();
+        BitVector in_bit(inst_count, false), out_bit(inst_count, false);
+        BitVector gen_bit = gen[inst], kill_bit = kill[inst];
+        auto oldOut = out[inst];
+        if(&bb->front() == inst) {
+          for(auto prev : predecessors(bb)) {
+            auto inst_prev = prev->getTerminator();
+            in_bit |= out[inst_prev];
+          }
+        } else {
+          in_bit = out[inst->getPrevNode()];
+        }
+        
+        out_bit = in_bit;
+        out_bit |= gen_bit;
+        out_bit &= kill_bit.flip();
+        in[inst] = in_bit;
+        out[inst] = out_bit;
+        /**
+        errs() << *inst << "\n" ;
+        for(int i = 0; i < gen[inst].size(); i++) {
+          errs() << gen[inst][i];
+        }
+        errs() << "\n";
+        for(int i = 0; i < kill[inst].size(); i++) {
+          errs() << kill[inst][i];
+        }
+        errs() << "\n";
+        for(int i = 0; i < in[inst].size(); i++) {
+          errs() << in[inst][i];
+        }
+        errs() << "\n";
+        for(int i = 0; i < out[inst].size(); i++) {
+          errs() << out[inst][i];
+        }
+        errs() << "\n";
+        **/
+        if(out_bit != oldOut) {
+          if(inst == bb->getTerminator()) {
+              for(auto next : successors(bb)) {
+                q.push(&next->front());
+              }
+          } else {
+            q.push(inst->getNextNode());
+          }
         }
       }
-      errs() << "}\n**************************************\n\n\n\n";
+    }
+
+    void printInAndOut(Function &F) {
+      for(auto &inst : instructions(F)) {
+        auto in_bit = in[&inst];
+        errs() << "INSTRUCTION: " << inst << "\n";
+        errs() << "***************** IN\n{\n";
+        for(unsigned i = 0; i < in_bit.size(); i++) {
+          if(in_bit[i] == true) errs() << " " << *label_inst[i] << "\n";
+        }
+        errs() << "}\n**************************************\n";
+        errs() << "***************** OUT\n{\n";
+        auto out_bit = out[&inst];
+        for(unsigned i = 0; i < out_bit.size(); i++) {
+          if(out_bit[i] == true) errs() << " " << *label_inst[i] << "\n";
+        }
+        errs() << "}\n**************************************\n\n\n\n";
+      }
     }
     // This function is invoked once per function compiled
     // The LLVM IR of the input functions is ready and it can be analyzed and/or transformed
@@ -104,14 +189,15 @@ namespace {
       setBitVector();
       for(auto &inst : instructions(F)) {
         if(isa<CallInst>(inst)) {
-          CallInst &cal_inst = cast<CallInst>(inst);
+          auto &cal_inst = cast<CallInst>(inst);
           setCluster(&cal_inst);
         }
       }
-
       for(auto &inst : instructions(F)) {
-          printGenAndKill(&inst);
+          setGenAndKill(&inst);
       }
+      setInAndOut(F);
+      printInAndOut(F);
       return false;
     }
 
